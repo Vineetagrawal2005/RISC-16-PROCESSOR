ca7f6754bfff39e097e1b7b3a42b60a7bbc75f85
Vineetagrawal2306 <vineetagrawal2306@gmail.com>
2026-03-11 09:12:05 +0530

    some important update

--- INSTRUCTION_MEM.v
+++ INSTRUCTION_MEM.v
@@ -27,11 +27,32 @@ module INSTRUCTION_MEM(
 
     reg [23:0] mem [65535:0];   // Memory array (depth can be adjusted)
     initial begin
-        // Example machine code
-        mem[0] = 24'hc10005; // MVI R1, 5
-        mem[1] = 24'hc20003; // MVI R2, 3
-        mem[2] = 24'h130120; // ADD R3, R1, R2
-        mem[3] = 24'h000000; // HLT (Enter halt state S9)
+       // --- DATA INITIALIZATION ---
+        mem[0]  = 24'hc1000A; // MVI R1, 10
+        mem[1]  = 24'hc20005; // MVI R2, 5 
+        
+        // --- ARITHMETIC & LOGIC ---
+        mem[2]  = 24'h130120; // ADD R3, R1, R2 (R3 = 15)
+        mem[3]  = 24'h240120; // SUB R4, R1, R2 (R4 = 5) 
+        mem[4]  = 24'h350120; // MUL R5, R1, R2 (R5 = 50)
+        mem[5]  = 24'h460120; // AND R6, R1, R2 (R6 = 0) 
+        mem[6]  = 24'h570120; // OR  R7, R1, R2 (R7 = 15)
+        mem[7]  = 24'h680120; // XOR R8, R1, R2 (R8 = 15)
+        
+        // --- SHIFTS & UNARY ---
+        mem[8]  = 24'h890100; // SHL R9, R1     (R9 = 20)
+        mem[9]  = 24'h9a0100; // SHR R10, R1    (R10 = 5)
+        mem[10] = 24'hab0100; // INC R11, R1    (R11 = 11)
+        mem[11] = 24'hbc0200; // DEC R12, R2    (R12 = 4)
+        
+        // --- MEMORY & BRANCHING ---
+        mem[12] = 24'he10064; // STORE R1, 100  (Store 10 at RAM[100])
+        mem[13] = 24'hdD0064; // LOAD R13, 100  (Load RAM[100] into R13)
+        mem[14] = 24'hf00002; // JUMP +2        (Skip next instruction)
+        mem[15] = 24'h1e0120; // ADD R14, R1, R2 (SKIPPED)
+        
+        // --- TERMINATION ---
+        mem[16] = 24'h000000; // HLT (Halt State S9)
     end
     // Instruction fetching logic
     assign instruction = mem[addr]; // Fetch instruction at PC address [cite: 69]
